c665b3c48ffd9303ed2599d681dd4973eca4697a
Kishanjayaprakash <kishanjayaprakash05@gmail.com>
2026-06-06 22:11:18 +0530

    ADD Print statement of server listening to port 8080

--- main.go
+++ main.go
@@ -9,7 +9,8 @@ func main() {
 	mux := http.NewServeMux() //router that matches incoming requests to their respective handlers
 
 	mux.HandleFunc("/", handleRoot) // routes "/" requests to handleRoot function
-
+    
+	fmt.Println("Server is running on http://localhost:8080") // prints a message to the console indicating that the server is running
 	http.ListenAndServe(":8080", mux) // starts the server and listens on port 8080 and sends requests to mux
 }
 
