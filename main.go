34177d0608088c7fe2673301da3c54e4bbbbd7dd
Kishanjayaprakash <kishanjayaprakash05@gmail.com>
2026-06-06 22:36:47 +0530

    Add simple Go REST API with JSON user creation and in-memory user cache

--- main.go
+++ main.go
@@ -3,17 +3,47 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"encoding/json"
 )
+
+type User struct {
+	Name string `json:"name"`
+
+}
+
+var usercache = make(map[int]User) // in-memory database, key is user id and value is User struct
 
 func main() {
 	mux := http.NewServeMux() //router that matches incoming requests to their respective handlers
 
 	mux.HandleFunc("/", handleRoot) // routes "/" requests to handleRoot function
-    
+
+    mux.HandleFunc( "POST /users", createUser) // routes "POST /users" requests to createUser function
+
 	fmt.Println("Server is running on http://localhost:8080") // prints a message to the console indicating that the server is running
-	http.ListenAndServe(":8080", mux) // starts the server and listens on port 8080 and sends requests to mux
+	err := http.ListenAndServe(":8080", mux)  
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+	}
 }
 
 func handleRoot(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello from root!")
+	fmt.Fprint(w, "Hello from root!") //sends get request response with "Hello from root!" message
+}
+
+
+// runs when POST /users is called
+func createUser(w http.ResponseWriter, r *http.Request) {
+	var user User
+	err := json.NewDecoder(r.Body).Decode(&user) // decodes the JSON request body into a User struct
+	if err != nil {
+		http.Error(w,err.Error(), http.StatusBadRequest)
+		return
+	}
+	if user.Name == "" {
+		http.Error(w, "Name is required", http.StatusBadRequest)
+		return
+	}
+	usercache[len(usercache)+1] = user // adds the user to the usercache map with a new id
+	w.WriteHeader(http.StatusNoContent) 
 }
