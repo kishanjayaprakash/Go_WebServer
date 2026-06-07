eb0c8ac21876472d7fb08143a283ac7a99a3f591
Kishanjayaprakash <kishanjayaprakash05@gmail.com>
2026-06-07 18:28:35 +0530

    Add GET /users route and handler

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 	"sync"
 )
 
@@ -22,6 +23,7 @@ func main() {
 	mux.HandleFunc("/", handleRoot) // routes "/" requests to handleRoot function
 
     mux.HandleFunc( "POST /users", createUser) // routes "POST /users" requests to createUser function
+    mux.HandleFunc("GET /users/{id}", getUser) // routes "GET /users/{id}" requests to getUser function
 
 	fmt.Println("Server is running on http://localhost:8080") // prints a message to the console indicating that the server is running
 	err := http.ListenAndServe(":8080", mux)  
@@ -34,6 +36,31 @@ func handleRoot(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello from root!") //sends get request response with "Hello from root!" message
 }
 
+func getUser(w http.ResponseWriter, r *http.Request) {
+	id, err:= strconv.Atoi(r.PathValue("id"))
+	if err != nil {
+		http.Error(w, "Invalid user ID", http.StatusBadRequest)
+		return
+	}
+	cachemutex.RLock() // locks the mutex for reading to allow multiple goroutines to read from the usercache map concurrently
+	user, ok := usercache[id]
+	cachemutex.RUnlock() // unlocks the mutex after reading from the map
+	if !ok {
+		http.Error(w, "User not found", http.StatusNotFound)
+		return
+	}
+
+    w.Header().Set("Content-Type", "application/json") // sets the Content-Type header to application/json to indicate that the response is in JSON format
+
+	j, err := json.Marshal(user) // marshals the User struct into JSON format
+	if err != nil {
+		http.Error(w, "Error encoding user data", http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK) // sets the HTTP status code to 200 OK
+	w.Write(j) // sends the JSON response back to the client
+	
+}
 
 // runs when POST /users is called
 func createUser(w http.ResponseWriter, r *http.Request) {
